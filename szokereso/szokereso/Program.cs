02b36b4a363e1dea3a13a8bbe97c529d868d8c9d
Konyicska-Kristof <113448136+Konyicska-Kristof@users.noreply.github.com>
2023-02-16 11:27:14 +0100

    Add files via upload

---
+++ szokereso/szokereso/Program.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace szokereso
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Szokereso> Szavak = new();
+
+            using StreamReader sr = new(@"..\..\..\src\szavak.txt", Encoding.UTF8);
+
+            while (!sr.EndOfStream)
+            {
+                Szavak.Add(new Szokereso(sr.ReadLine()));
+            }
+            
+            
+            Console.WriteLine($"1. Feladat - Szavak száma: {Szavak.Count} db");
+
+            int im = 0;
+            int max = int.MinValue;
+            while (im<Szavak.Count)
+            {
+                if (Szavak[im].Szo.Length>max)
+                {
+                    max = Szavak[im].Szo.Length;
+                }
+                im++;
+            }
+            
+            Console.WriteLine($"2. Feladat - Leghosszabb szó hossza: {max} db karakter");
+
+            Console.WriteLine("3. Feladat - Leghosszabb szó/szavak:");
+
+            for (int i = 0; i < Szavak.Count; i++)
+            {
+                if (Szavak[i].Szo.Length==max)
+                {
+                    Console.Write($"\t{Szavak[i].Szo}\n");
+                }
+            }
+
+            Console.Write("5. Feladat - Szavak kiírása\n\t");
+
+            string betu = "#";
+            for (int i = 0; i < 16; i++)
+            {               
+                for (int j = 0; j < 16; j++)
+                {
+                   //karakter szelektor
+                    Console.Write($"{betu}");
+                    if (j == 15)
+                    {
+                        Console.Write("\n\t");
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ szokereso/szokereso/Szokereso.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace szokereso
+{
+    internal class Szokereso
+    {
+        public string Szo;
+        public int S_index;
+        public int O_index;
+        public int Irany;
+
+        public Szokereso(string Sor)
+        {
+            var dbok = Sor.Split('*');
+
+            Szo = dbok[0];
+            S_index = int.Parse(dbok[1]);
+            O_index = int.Parse(dbok[2]);
+            Irany = int.Parse(dbok[3]);
+        }
+
+    }
+}
